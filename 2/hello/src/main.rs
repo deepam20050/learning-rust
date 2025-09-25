fed5e28a21355e17ffd0b06b3c0e359a34214854
Deepam Sarmah <deepam20050@iiitd.ac.in>
2025-09-25 21:48:23 +0530

    Added gcd function

--- 2/hello/src/main.rs
+++ 2/hello/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+    let m: u64 = 10;
+    let n: u64 = 20;
+    println!("{}", gcd(n, m));
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
