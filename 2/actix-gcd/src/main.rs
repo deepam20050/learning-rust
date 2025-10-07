589a6e688adad2fcc24f4426107dfd5469cbcc01
Deepam Sarmah <deepamsarmah.cpp@gmail.com>
2025-10-07 11:25:10 +0530

    done with actix gcd with POST

--- 2/actix-gcd/src/main.rs
+++ 2/actix-gcd/src/main.rs
@@ -1,11 +1,12 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
-
+use serde::Deserialize;
 
 #[actix_web::main]
 async fn main() {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("Serving on http:localhost:3000...");
@@ -15,6 +16,25 @@ async fn main() {
         .run()
         .await
         .expect("error running server");
+}
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+                .content_type("text/html")
+                .body("Computing the GCD with 0 is invalid");
+    }
+    let response = format!("The GCD of {} and {} \
+                                    is <b>{}</b\n", form.n, form.m, gcd(form.m, form.n));
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)                                
 }
 
 async fn get_index() -> HttpResponse {
@@ -30,4 +50,18 @@ async fn get_index() -> HttpResponse {
                 </form>
             "#,
         )
+}
+
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
