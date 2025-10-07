b2a84b912f2ab116da93be536874aa8967425db2
Deepam Sarmah <deepamsarmah.cpp@gmail.com>
2025-10-07 21:52:47 +0530

    Used a reference to fix copying

---
+++ 4/copies/src/main.rs
@@ -0,0 +1,17 @@
+struct Label {
+    number: u32,
+}
+
+/*
+    I borrowed a reference and so was able to print
+*/
+fn print(l: &Label) -> () {
+    println!("STAMP: {}", l.number);
+}
+
+fn main() {
+    let l = Label { number : 3};
+    print(&l);
+    // I borrowed a reference so was able to print
+    println!("My label number is: {}", l.number);
+}
