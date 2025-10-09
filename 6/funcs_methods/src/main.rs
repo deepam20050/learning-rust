5633f6d077b8f1e17df7bc1b60718aa6e05c4c7a
Deepam Sarmah <deepamsarmah.cpp@gmail.com>
2025-10-09 11:29:47 +0530

    exclusive and inclusive range code

--- 6/funcs_methods/src/main.rs
+++ 6/funcs_methods/src/main.rs
@@ -7,4 +7,10 @@ fn main() {
 
     // <T> doesn't work
     // we need to use ::<T> 
+
+    let a: i32 = 1;
+    let b: i32 = 10;
+
+    let rangeA = a..b; // end exclusive
+    let rangeB = a..=b; // end inclusive
 }
