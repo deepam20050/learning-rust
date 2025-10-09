3535e0022b3a2f5d445848fb60f702ed3e15eed5
Deepam Sarmah <deepamsarmah.cpp@gmail.com>
2025-10-09 11:44:15 +0530

    Learna about closures

--- 6/funcs_methods/src/main.rs
+++ 6/funcs_methods/src/main.rs
@@ -13,4 +13,20 @@ fn main() {
 
     let rangeA = a..b; // end exclusive
     let rangeB = a..=b; // end inclusive
+
+    /*
+        Closures in Rust
+     */
+
+    let is_even = |x| x % 2 == 0;
+
+    let is_even_verbose = |x: u64| -> bool {
+        x % 2 == 0
+    };
+
+    println!("{}", is_even(5));
+    println!("{}", is_even(4));
+    
+    println!("{}", is_even_verbose(5));
+    println!("{}", is_even_verbose(4));
 }
