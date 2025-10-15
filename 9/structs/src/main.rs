f7e389cb582dfe523f77602f69d3c97300cb4aa2
Deepam Sarmah <deepamsarmah.cpp@gmail.com>
2025-10-15 23:39:37 +0530

    Add examples of struct types and usage

--- 9/structs/src/main.rs
+++ 9/structs/src/main.rs
@@ -17,6 +17,53 @@ fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap {
     GrayscaleMap { pixels, size }
 }
 
+struct Broom {
+    name: String,
+    height: u32,
+    health: u32,
+    position: (f32, f32, f32),
+    intent: BroomIntent,
+}
+
+#[derive(Clone, Copy)]
+enum BroomIntent {
+    FetchWater,
+    DumpWater,
+}
+
+fn chop(b: Broom) -> (Broom, Broom) {
+    let mut broom1 = Broom {
+        height: b.height / 2,
+        ..b
+    };
+
+    // String doesn't support Copy
+    // String supports clone
+    let mut broom2 = Broom {
+        name: broom1.name.clone(),
+        ..broom1
+    };
+
+    broom1.name.push_str(" I");
+    broom2.name.push_str(" II");
+
+    (broom1, broom2)
+}
+
+// this is a tuple like struct
+struct Bounds(usize, usize);
+/*
+ * Individual elements of a tuple-like struct maybe public or not
+ */
+ 
+// newtypes - structs with a single component that you define
+// to get a stricter type checking
+struct Ascii(Vec<u8>);
+
+// unit-like structs
+// no memory allocation or machine code is generated
+struct Onesuch;
+
 fn main() {
     let width = 1024;
     let height = 576;
@@ -26,4 +73,21 @@ fn main() {
     };
     assert_eq!(image.pixels.len(), width * height);
     assert_eq!(image.size.0 * image.size.1, width * height);
+
+    let hokey = Broom {
+        name: String::from("Hokey"),
+        height: 60,
+        health: 100,
+        position: (100.0, 200.0, 0.0),
+        intent: BroomIntent::FetchWater,
+    };
+
+    let (hokey1, hokey2) = chop(hokey);
+    assert_eq!(hokey1.name, "Hokey I");
+    assert_eq!(hokey2.name, "Hokey II");
+
+    let image_bounds = Bounds(1024, 768);
+    assert_eq!(image_bounds.0 * image_bounds.1, 1024 * 768);
+    
+    let o = Onesuch;
 }
