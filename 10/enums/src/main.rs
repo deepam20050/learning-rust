d51bd19ae20eee568cb7ec45c767b92c3f47ad12
Deepam Sarmah <deepamsarmah.cpp@gmail.com>
2025-10-15 13:50:35 +0530

    learnt about enums

--- 10/enums/src/main.rs
+++ 10/enums/src/main.rs
@@ -5,8 +5,8 @@
 // }
 
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::mem::size_of;
-use std::time;
 
 fn compare(n: i32, m: i32) -> Ordering {
     if n < m {
@@ -63,6 +63,49 @@ impl TimeUnit {
     }
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum RoughTime {
+    // These are called tuple variants
+    InThePast(TimeUnit, u32),
+    JustNow,
+    InTheFuture(TimeUnit, u32),
+}
+
+enum Shape {
+    Rectangle { lenght: u32, width: u32 },
+}
+
+enum Json {
+    Null,
+    Boolean(bool),
+    Number(f64),
+    String(String),
+    Array(Vec<Json>),
+    Object(Box<HashMap<String, Json>>),
+}
+
+// Generic types
+// enum Option<T> {
+//     None,
+//     Some(T),
+// }
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+
+// Generic data structures
+enum BinaryTree<T> {
+    Empty,
+    NonEmpty(Box<TreeNode<T>>),
+}
+
+struct TreeNode<T> {
+    element: T,
+    left: BinaryTree<T>,
+    right: BinaryTree<T>,
+}
+
 fn main() {
     assert_eq!(HttpStatus::Ok as i32, 200);
     assert_eq!(size_of::<HttpStatus>(), 2);
@@ -78,4 +121,21 @@ fn main() {
     let time_unit = TimeUnit::Days;
     println!("{}", time_unit.plural());
     println!("{}", time_unit.singular());
+
+    let four_score_and_seven_years_ago = RoughTime::InThePast(TimeUnit::Years, 4 * 20 + 7);
+
+    let three_hours_from_now = RoughTime::InTheFuture(TimeUnit::Hours, 3);
+
+    let square = Shape::Rectangle {
+        lenght: 5,
+        width: 5,
+    };
+
+    let jupiter_tree = BinaryTree::NonEmpty(Box::new(TreeNode {
+        element: "Jupiter",
+        left: BinaryTree::Empty,
+        right: BinaryTree::Empty,
+    }));
+    
+    
 }
