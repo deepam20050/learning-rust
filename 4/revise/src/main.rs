b45c6216338f3ec4be1fa511332cffd7ceeb2d77
Deepam Sarmah <deepamsarmah.cpp@gmail.com>
2025-10-16 16:01:03 +0530

    Revised chapter 4

--- 4/revise/src/main.rs
+++ 4/revise/src/main.rs
@@ -10,8 +10,12 @@ fn main() {
         *x += 1;
         println!("==> {}", x);
     }
-    // for &x in &mut v {
-    //     *x += 1;
-    //     println!("<<= {}", x);
-    // }
+    println!("{}", v.len());
+    println!("{}", v.len());
+
+    /*
+     * println!() borrows its arguments. It doesn't take ownership unless you
+     * do something like this:
+     */
+    println!("{}", "hello !!".to_string()); // but this "hello" String is temporary so you wouldn't be able to use it anyway
 }
