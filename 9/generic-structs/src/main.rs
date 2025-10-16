33b09784a412885bcc36094dd30d51a31b29b00a
Deepam Sarmah <deepamsarmah.cpp@gmail.com>
2025-10-16 12:11:37 +0530

    Add Extrema struct and find_extrema

--- 9/generic-structs/src/main.rs
+++ 9/generic-structs/src/main.rs
@@ -35,8 +35,35 @@ impl<T> Queue<T> {
     }
 }
 
+// structs with lifetime parameters
+struct Extrema<'elt> {
+    greatest: &'elt i32,
+    least: &'elt i32,
+}
+
+fn find_extrema<'s>(slice: &'s [i32]) -> Extrema<'s> {
+    let mut greatest = &slice[0];
+    let mut least = &slice[0];
+
+    for i in 1..slice.len() {
+        if slice[i] < *least {
+            least = &slice[i];
+        }
+        if (slice[i] > *greatest) {
+            greatest = &slice[i];
+        }
+    }
+
+    Extrema { greatest, least }
+}
+
 fn main() {
     // turbo fish lessgoo
     let mut q = Queue::<char>::new();
     assert!(q.is_empty());
+
+    let a = [0, -3, 0, 15, 48];
+    let e = find_extrema(&a);
+    assert_eq!(*e.least, -3);
+    assert_eq!(*e.greatest, 48);
 }
